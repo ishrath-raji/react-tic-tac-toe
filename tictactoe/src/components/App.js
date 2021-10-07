01ae6dbabc711c465d717e141ce0149421a27fe9
Ishrath Raji <ishrathraji8@gmail.com>
2021-10-07 12:05:22 +0530

    added a layout

--- tictactoe/src/components/App.js
+++ tictactoe/src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const App = () => {
-  return <div>MY APP NOWWWWWWW</div>;
+  return (
+    <div>
+      <h1>MY APP NOWWWWWWW</h1>
+      <Container>
+        <Row>
+          <Col sm={8}>Intro</Col>
+          <Col sm={4}>Leaderboard</Col>
+        </Row>
+        <Row>
+          <Col sm>Recently Played</Col>
+          <Col sm>Comments and Reviews </Col>
+          <Col sm>......</Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default App;
