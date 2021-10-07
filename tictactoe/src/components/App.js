94f3164f4c4a8461f052d5bd8dddee0033056b12
Jude <j.k.a.bandara@gmail.com>
2021-10-07 12:21:36 +0530

    umm

--- tictactoe/src/components/App.js
+++ tictactoe/src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
+import {Container} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 
 const App = () => {
   return <div>
     <Container>
       <Row>
-        <Col>1 of 1</Col>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col> 1 of 3</Col>
+        <Col> 2 of 3</Col>
+        <Col> 3 of 3</Col>
       </Row>
     </Container></div>;
 };
