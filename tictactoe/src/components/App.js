e7405ddc4cafbf47690184d40dc7507d2b2f866c
Jude <j.k.a.bandara@gmail.com>
2021-10-07 14:04:08 +0530

    skeleton folder

--- tictactoe/src/components/App.js
+++ tictactoe/src/components/App.js
@@ -1,32 +1,36 @@
+import "../App.css";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import "../App.css";
+import Description from "./homeSubComponents/Description";
+import RecentlyPlayed from "./homeSubComponents/RecentlyPlayed";
+import Leaderboard from "./homeSubComponents/Leaderboard";
+import Comments from "./homeSubComponents/Comments";
 
 const App = () => {
   return (
     <div>
       <Container style={{ margin: 10 }} fluid={true}>
         <Row>
-          <Col sm={6} md={7}>
+          <Col sm={7} md={7}>
             <Container>
               <Row>
                 <Col className="intro" sm={12} md={12}>
-                <div className="intro box">TIC-TAC-TOE</div>
+                <div className="intro box"><Description /></div>
                 </Col>
               </Row>
               <Row>
                 <Col className="recently-played" sm={6} md={6}>
-                  <div className="rp box">Recently Played</div>
+                  <div className="rp box"><RecentlyPlayed /></div>
                 </Col>
                 <Col className="comments" sm={6} md={6}>
-                  <div className="comments box"> Comments and Reviews </div>
+                  <div className="comments box"> <Comments /> </div>
                 </Col>
               </Row>
             </Container>
           </Col>
 
-          <Col className="leaderboard" sm={5} md={4}>
-          <div className="lb box">Leaderboard</div>
+          <Col className="leaderboard" sm={4} md={4}>
+          <div className="lb box"><Leaderboard /></div>
           </Col>
         </Row>
       </Container>
