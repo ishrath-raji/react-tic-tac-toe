e99de51fe15f1aee40465df5a6793d569ec93248
Jude <j.k.a.bandara@gmail.com>
2021-10-07 11:57:01 +0530

    added a container to app.js

--- tictactoe/src/components/App.js
+++ tictactoe/src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import container from 'react-bootstrap/Container';
 
 const App = () => {
-  return <div>MY APP NOWWWWWWW</div>;
+  return <div>
+    <Container>
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container></div>;
 };
 
 export default App;
