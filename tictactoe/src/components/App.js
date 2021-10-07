87353b19f2cd6351239214fbe4fb30422e5e9578
Naduni Prabashwari Ranasinghe <64697697+naduniprabhashwari@users.noreply.github.com>
2021-10-07 11:35:41 +0530

    new change h1

--- tictactoe/src/components/App.js
+++ tictactoe/src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>MY APP NOWWWWWWW</div>;
+  return (
+    <div>
+      MY APP NOWWWWWWW
+      <h1>new edit check</h1>
+    </div>
+  );
 };
 
 export default App;
