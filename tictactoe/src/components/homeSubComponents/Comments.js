ee891d57cdf707fbad01221d2f7d7599c1666bcc
Naduni Prabashwari Ranasinghe <64697697+naduniprabhashwari@users.noreply.github.com>
2021-10-09 17:26:11 +0530

    Comment section update

--- tictactoe/src/components/homeSubComponents/Comments.js
+++ tictactoe/src/components/homeSubComponents/Comments.js
@@ -1,36 +1,97 @@
-import "./comment.css";
+import ".../App.css";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { Comment, Avatar, Button, List, Input, Rate } from "antd";
 
-const Comments = () => {
-  return (
-    <div className="comment-body">
-      <h3>Comments & Review </h3>
-      <Container style={{ padding: "0" }}>
-        <Row style={{ marginLeft: "0%", marginRight: "0%" }}>
-          <Col className="col-box avatar" xs={10} md={2}>
-            <svg
-              width="45"
-              height="45"
-              viewBox="0 0 51 51"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="25.5" cy="25.5" r="25.5" fill="#C4C4C4" />
-            </svg>
-          </Col>
-          <Col className="col-box text" xs={6} md={10}>
-            <form>
-              <div className="input-type-box">
-                <input type="text" className="input-text"></input>
-                <button className="button">Post</button>
-              </div>
-            </form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
+const CommentList = ({ comments }) => (
+  <List
+    dataSource={comments}
+    itemLayout="horizontal"
+    renderItem={(props) => <Comment {...props} />}
+  />
+);
+
+class Comments extends React.Component {
+  state = {
+    comments: [],
+    submitting: false,
+    value: "",
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.value);
+    if (!this.state.value) {
+      return;
+    }
+
+    this.setState({
+      submitting: true,
+    });
+
+    this.setState({
+      submitting: false,
+      value: "",
+      comments: [
+        ...this.state.comments,
+        {
+          author: "User name",
+          avatar:
+            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+          content: <p>{this.state.value}</p>,
+          rate: <Rate disable allowHalf defaultValue={4} />,
+        },
+      ],
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  render() {
+    const { comments, submitting, value } = this.state;
+    return (
+      <div className="comment-body">
+        <h3>Comments & Review </h3>
+        <Container style={{ padding: "0" }}>
+          <Row style={{ marginLeft: "0%", marginRight: "0%" }}>
+            <Col className="col-box avatar" xs={10} md={2}>
+              <Avatar
+                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                alt="user_name"
+              />
+            </Col>
+            <Col className="col-box text" xs={6} md={10}>
+              <form>
+                <div className="input-type-box">
+                  <Input
+                    className="input-text"
+                    onChange={this.handleChange}
+                    value={value}
+                  />
+                  <Button
+                    htmlType="submit"
+                    loading={submitting}
+                    onClick={this.handleSubmit}
+                    className="comment-button"
+                  >
+                    Post
+                  </Button>
+                </div>
+              </form>
+            </Col>
+          </Row>
+          <Row className="comment-list">
+            <CommentList comments={comments} />
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Comments;
