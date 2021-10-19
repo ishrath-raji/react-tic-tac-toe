b9807087d2716c8ca4a1e3409550bf6d27249088
Naduni Prabashwari Ranasinghe <64697697+naduniprabhashwari@users.noreply.github.com>
2021-10-19 13:01:52 +0530

    remove popup to recreate

--- tictactoe/src/components/homeSubComponents/Comments.js
+++ tictactoe/src/components/homeSubComponents/Comments.js
@@ -1,32 +1,29 @@
-/* eslint-disable react/jsx-pascal-case */
 import "../../App.css";
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { Avatar, Button, Rate } from "antd";
-import CommentPopup from "./CommentPopup";
+import { Avatar, Button, Input, Rate } from "antd";
 
 const CommentList = ({ comments }) => {
   const listItems = comments.map((comment) => {
-    return <div>{comments.lenght}</div>;
-    // return (
-    //   <Container key={comment.author}>
-    //     <Row className="row comment">
-    //       <Col className="col-box comment avatar" xs={10} md={2}>
-    //         <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-    //       </Col>
-    //       <Col className="col-box" xs={6} md={10}>
-    //         <div>
-    //           <b>name</b> 4.5
-    //           <br />
-    //           <p>well dome</p>
-    //         </div>
-    //       </Col>
-    //     </Row>
-    //   </Container>
-    // );
+    return (
+      <Container key={comment.author}>
+        <Row className="row comment">
+          <Col className="col-box comment avatar" xs={10} md={2}>
+            <Avatar src={comment.avatar} />
+          </Col>
+          <Col className="col-box" xs={6} md={10}>
+            <div>
+              <b>{comment.author}</b> {comment.rate}
+              <br />
+              {comment.content}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
   });
 
-  return <React.Fragment>{console.log(comments.lenght)}</React.Fragment>;
+  return <React.Fragment>{listItems}</React.Fragment>;
 };
 
 class Comments extends React.Component {
@@ -34,44 +31,11 @@ class Comments extends React.Component {
     comments: [],
     submitting: false,
     value: "",
-    popupData: [],
+    commentListHeight: 0,
   };
 
-  // handleSubmit = (event) => {
-  //   event.preventDefault();
-
-  //   if (!this.state.value) {
-  //     return;
-  //   }
-
-  //   this.setState({
-  //     submitting: true,
-  //   });
-
-  //   this.setState({
-  //     submitting: false,
-  //     value: "",
-  //     comments: [
-  //       ...this.state.comments,
-  //       {
-  //         author: "User name",
-  //         avatar:
-  //           "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-  //         content: <p>{this.state.value}</p>,
-  //         rate: <Rate disabled allowHalf defaultValue={4.5} />,
-  //       },
-  //     ],
-  //   });
-  // };
-
-  // handleChange = (e) => {
-  //   this.setState({
-  //     value: e.target.value,
-  //   });
-  // };
-
-  handlePopupData = (popupData) => {
-    this.state.popupData = popupData;
+  handleSubmit = (event) => {
+    event.preventDefault();
 
     if (!this.state.value) {
       return;
@@ -83,32 +47,28 @@ class Comments extends React.Component {
 
     this.setState({
       submitting: false,
+      value: "",
       comments: [
         ...this.state.comments,
         {
           author: "User name",
           avatar:
             "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-          content: this.state.popupData.feedback,
-          rate: (
-            <Rate
-              disabled
-              allowHalf
-              defaultValue={this.state.popupData.rateValue}
-            />
-          ),
+          content: <p>{this.state.value}</p>,
+          rate: <Rate disabled allowHalf defaultValue={4.5} />,
         },
       ],
     });
-    this.state.comments.author = "User name";
-    this.state.comments.avatar =
-      "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png";
-    this.state.comments.content = this.state.popupData.feedback;
-    this.state.comments.rate = this.state.popupData.rateValue;
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
   };
 
   render() {
-    const { comments } = this.state;
+    const { comments, submitting, value } = this.state;
     return (
       <div className="comment-body">
         <h3>Comments & Review </h3>
@@ -123,8 +83,20 @@ class Comments extends React.Component {
             <Col className="col-box text" xs={6} md={10}>
               <form>
                 <div className="input-type-box">
-                  <CommentPopup handlePopupData={this.handlePopupData} />
-                  <Button className="comment-button">Post</Button>
+                  <Input
+                    className="input-text"
+                    onChange={this.handleChange}
+                    value={value}
+                    placeholder="Comment"
+                  />
+                  <Button
+                    htmlType="submit"
+                    loading={submitting}
+                    onClick={this.handleSubmit}
+                    className="comment-button"
+                  >
+                    Post
+                  </Button>
                 </div>
               </form>
             </Col>
