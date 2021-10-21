b4b36ce67a372bb3366eaa28dbd6c76388bec111
Naduni Prabashwari Ranasinghe <64697697+naduniprabhashwari@users.noreply.github.com>
2021-10-21 13:07:10 +0530

    finished single player modal

--- tictactoe/src/components/Menu.js
+++ tictactoe/src/components/Menu.js
@@ -1,8 +1,7 @@
 import React from "react";
 import logo from "../icons/tic-tac-toe-logo.png";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Modal, Row, Radio } from "antd";
-import SinglePlayer from "./SinglePlayer";
 
 const style = {
   width: "100%",
@@ -12,18 +11,10 @@ const style = {
   alignItems: "center",
 };
 
-var difficulty = false;
-var playerSymbol = false;
-
-function redirectingPage() {
-  <Redirect to="/gamehub/tictactoe/play/singleplayer"></Redirect>;
-}
-
 class Menu extends React.Component {
   state = {
     modalVisible: false,
     onDifficulty: false,
-    onPlayer: false,
   };
 
   setModalVisible(modalVisible) {
@@ -32,26 +23,6 @@ class Menu extends React.Component {
 
   setDifficulty(onDifficulty) {
     this.setState({ onDifficulty: onDifficulty });
-    console.log("true diff");
-  }
-
-  setSymbol(onPlayer) {
-    this.setState({ onPlayer: onPlayer });
-    console.log("true player");
-  }
-
-  Clicked(button) {
-    if (button === "easy" || button === "hard") {
-      difficulty = true;
-    }
-    if (button === "X" || button === "O") {
-      playerSymbol = true;
-    }
-
-    if (playerSymbol && difficulty) {
-      console.log("all true");
-      redirectingPage();
-    }
   }
 
   render() {
@@ -97,24 +68,10 @@ class Menu extends React.Component {
                     this.setDifficulty(true);
                   }}
                 >
-                  {/* <Col className="col button left"> */}
-                  <Radio.Button
-                    value="easy"
-                    className="button difficulty easy"
-                    onClick={() => {
-                      this.Clicked("easy");
-                    }}
-                  >
+                  <Radio.Button value="easy" className="button difficulty easy">
                     Easy
                   </Radio.Button>
-                  {/* <Col className="col button"> */}
-                  <Radio.Button
-                    value="hard"
-                    className="button difficulty hard"
-                    onClick={() => {
-                      this.Clicked("hard");
-                    }}
-                  >
+                  <Radio.Button value="hard" className="button difficulty hard">
                     Hard
                   </Radio.Button>
                 </Radio.Group>
@@ -122,36 +79,24 @@ class Menu extends React.Component {
               <Row>
                 <h5 className="modal-title">SELECT SYMBOL</h5>
               </Row>
-              <Row className="row button">
-                <Radio.Group
-                  onChange={() => {
-                    this.setSymbol(true);
-                  }}
-                >
-                  {/* <Col className="col button left"> */}
-
-                  <Radio.Button
-                    value="X"
-                    className="button symbol X"
-                    onClick={() => {
-                      this.Clicked("X");
-                    }}
-                  >
-                    X
-                  </Radio.Button>
-                  {/* <Col className="col button"> */}
-
-                  <Radio.Button
-                    value="O"
-                    className="button symbol O"
-                    onClick={() => {
-                      this.Clicked("O");
-                    }}
-                  >
-                    O
-                  </Radio.Button>
-                </Radio.Group>
-              </Row>
+              <Link
+                to={
+                  this.state.onDifficulty
+                    ? "/gamehub/tictactoe/play/singleplayer"
+                    : "/gamehub/tictactoe/play"
+                }
+              >
+                <Row className="row button">
+                  <Radio.Group>
+                    <Radio.Button value="X" className="button symbol X">
+                      X
+                    </Radio.Button>
+                    <Radio.Button value="O" className="button symbol O">
+                      O
+                    </Radio.Button>
+                  </Radio.Group>
+                </Row>
+              </Link>
             </div>
           </Modal>
         </div>
