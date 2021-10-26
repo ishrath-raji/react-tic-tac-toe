46bf53d7829f5f4a603ab19c8e13099eafdc01f3
Naduni Prabashwari Ranasinghe <64697697+naduniprabhashwari@users.noreply.github.com>
2021-10-26 13:38:25 +0530

    pass player turn

--- tictactoe/src/singlePlayerSubComponents/PlayerCard.js
+++ tictactoe/src/singlePlayerSubComponents/PlayerCard.js
@@ -1,49 +1,53 @@
 import "../App.css";
 import React, { useEffect, useState } from "react";
-import {players} from "../components/homeSubComponents/RecentlyPlayed";
-import {AITurn} from "./GameBoard";
+import { players } from "../components/homeSubComponents/RecentlyPlayed";
+import { AITurn } from "./GameBoard";
 
+const PlayerCard = ({ turn }) => {
+  const [AITurn, setAITurn] = useState("");
 
-const PlayerCard = () => {
-   const [AITurn, setAITurn] = useState("");
+  const [playerName, setPlayerName] = useState("");
+  const [playerAvatar, setPlayerAvatar] = useState("");
+  const [playerTitle, setPlayerTitle] = useState("");
+  const [playerEmail, setPlayerEmail] = useState("");
+  const [playerTel, setPlayerTel] = useState("");
+  const number = Math.floor(Math.random() * (players.length - 1) + 1);
 
-   const[playerName, setPlayerName] = useState("");
-   const[playerAvatar, setPlayerAvatar] = useState("");
-   const[playerTitle, setPlayerTitle] = useState("");
-   const[playerEmail, setPlayerEmail] = useState("");
-   const[playerTel,setPlayerTel] = useState("");
-   const number =  Math.floor(Math.random()* (players.length - 1) + 1);
+  console.log(turn);
 
-    // need to use useffect 
-    const profileData = async () =>{
-       try{
-          const profile = players[number]
-          setPlayerAvatar(profile.src)
-          setPlayerName(profile.name)
-          setPlayerTitle(profile.title)
-          setPlayerEmail(profile.email)
-          setPlayerTel(profile.tel)
-          setAITurn(true)
-       }catch(error){
-          console.log(error)
-       }
+  // need to use useffect
+  const profileData = async () => {
+    try {
+      const profile = players[number];
+      setPlayerAvatar(profile.src);
+      setPlayerName(profile.name);
+      setPlayerTitle(profile.title);
+      setPlayerEmail(profile.email);
+      setPlayerTel(profile.tel);
+      setAITurn(true);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-       profileData();
-    },[])
-    return (
-       <div className="player-card-container">
-            <div className={`player-card ${AITurn? "" : "aiturn"}`}>
-                
-               <p className="player-avatar"><img src={playerAvatar} width="110"/></p>
-               <h5>{playerName}</h5>
-               <p>{playerTitle}</p> 
-               <p className="player-contact">{playerEmail} {playerTel}</p>
-               {/* <p className="player-status">active</p><br/> */}
-              </div>
-       </div>
-    )
-}
+  useEffect(() => {
+    profileData();
+  }, []);
+  return (
+    <div className="player-card-container">
+      <div className={`player-card ${turn === "O" ? "" : "aiturn"}`}>
+        <p className="player-avatar">
+          <img src={playerAvatar} width="110" />
+        </p>
+        <h5>{playerName}</h5>
+        <p>{playerTitle}</p>
+        <p className="player-contact">
+          {playerEmail} {playerTel}
+        </p>
+        {/* <p className="player-status">active</p><br/> */}
+      </div>
+    </div>
+  );
+};
 
 export default PlayerCard;
