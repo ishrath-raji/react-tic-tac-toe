74fa3a7d907ba27046f16bf673f3c1b3a0f5e3f3
Ishrath Raji <ishrathraji8@gmail.com>
2021-11-17 09:24:22 +0530

    added the multiplayer gameboard

--- tictactoe/src/components/MultiPlayer.js
+++ tictactoe/src/components/MultiPlayer.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Col, Row } from "react-bootstrap";
+import GameBoard from "./singlePlayerSubComponents/GameBoard";
 
 const style = {
   border: "1px solid black",
 };
 
 const MultiPlayer = () => {
+  const [playerTurn, setPlayerTurn] = useState("X");
+
+  const ChangeplayerTurn = (playturn) => {
+    setPlayerTurn(playturn);
+  };
+
   return (
     <div>
       <Container fluid={true}>
@@ -14,7 +21,19 @@ const MultiPlayer = () => {
             1st player card
           </Col>
           <Col md={4} style={style}>
-            Game board
+            <div className="mp-title">
+              <span className="title-green">Tic</span>-
+              <span className="title-orange">Tac</span>-
+              <span className="title-green">Toe</span>
+            </div>
+            <div className="mp-gameboard">
+              <GameBoard
+                rounds={3}
+                symbol={"X"}
+                difficulty={"easy"}
+                turn={ChangeplayerTurn}
+              />
+            </div>
           </Col>
           <Col md={4} style={style}>
             2nd player card
